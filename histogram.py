82d77e398b2cd072d3ee170a0394b63cbf15eeb8
Brandon <bapon@umail.ucsb.edu>
2017-10-13 12:45:36 -0700

    plotting histograms

--- histogram.py
+++ histogram.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 import numpy as numpy
 import sys
 import math
@@ -24,7 +24,23 @@ class histogram:
 
 	def make_histogram(self, num_output, current_bin_list):
 		#outputs histogram with name[num_output] as title
-		pass
+		bin_list = []
+		for i in range(0,self.num_bins):
+			bin_list.append(i)
+
+		fig = plt.figure(1)
+		print ("self.num_bins =", self.num_bins)
+		plt.bar(bin_list, current_bin_list, color = "blue")
+
+		plt.xlabel('Bins')
+		plt.ylabel('Number of Samples')
+		plt.title(r'$\mathrm{Histogram\ of\ IQ:}\ \mu=100,\ \sigma=15$') #fix this
+		plt.axis([-1, 10, 0, 1500])
+		plt.xticks(range(10))
+		plt.tick_params(axis=u'both', which=u'both',length=0)
+		plt.grid(True)
+		name = self.title_name + '[' + str(num_output) + ']'
+		fig.savefig(name)
 
 	def output_all_histograms(self):
 		current_bin_list = []
@@ -36,6 +52,7 @@ class histogram:
 				current_bin_list.append(self.input_bins[self.num_histograms*j+i])
 				j+=1
 			print("Current_bin_list = ", current_bin_list)
+			self.make_histogram(i, current_bin_list)
 			i+=1
 			del current_bin_list[:]
 			#clear List
